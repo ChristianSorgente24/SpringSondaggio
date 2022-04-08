0c2ceebaf0d949b1821c13ec1c2cea28a65c24a5
Alessandro Trezzi <Alessandro Trezzi@LAPTOP-JHO38MNT.homenet.telecomitalia.it>
2022-04-08 16:08:01 +0200

    SondaggioService notation

--- sondaggio/src/main/java/com/milano/sondaggio/service/impl/SondaggioServiceImpl.java
+++ sondaggio/src/main/java/com/milano/sondaggio/service/impl/SondaggioServiceImpl.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.milano.sondaggio.model.Sondaggio;
 import com.milano.sondaggio.repository.SondaggioRepository;
 import com.milano.sondaggio.service.SondaggioService;
 
-
+@Service("sondaggioService")
 public class SondaggioServiceImpl implements SondaggioService {
 
 	@Autowired
