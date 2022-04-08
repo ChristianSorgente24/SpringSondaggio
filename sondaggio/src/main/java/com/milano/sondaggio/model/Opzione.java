0118af677a8487967738ebf6ab3b32ff4a4b52e4
punta <punta@DESKTOP-IKA4HNR.homenet.telecomitalia.it>
2022-04-08 16:09:09 +0200

    Model Opzione

---
+++ sondaggio/src/main/java/com/milano/sondaggio/model/Opzione.java
@@ -0,0 +1,33 @@
+package com.milano.sondaggio.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="opzione")
+public class Opzione implements Serializable{
+	
+	private static final long serialVersionUID = -3631495711562326064L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="id_sondaggio")
+	private Sondaggio sondaggio;
+	
+	
+	private String opzione;
+}
