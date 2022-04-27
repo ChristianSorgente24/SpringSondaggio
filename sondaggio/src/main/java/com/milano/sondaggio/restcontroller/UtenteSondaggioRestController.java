25fc96f09f0a41c98b0298de70bb9ebdeb4aa9f2
puntiss <puntaandrea@gmail.com>
2022-04-27 12:15:46 +0200

    ljjl

--- sondaggio/src/main/java/com/milano/sondaggio/restcontroller/UtenteSondaggioRestController.java
+++ sondaggio/src/main/java/com/milano/sondaggio/restcontroller/UtenteSondaggioRestController.java
@@ -35,6 +35,7 @@ public class UtenteSondaggioRestController {
 	@Autowired
 	private SondaggioService sondaggioService;
 
+	
 	@GetMapping("/canVoto/{id_sondaggio}")
 	public long canVoto(@PathVariable long id_sondaggio, HttpSession session) {
 
