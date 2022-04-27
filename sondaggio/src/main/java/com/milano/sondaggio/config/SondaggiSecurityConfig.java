27817f084d195ee0ddd91b0eb3b69a2c81a403d3
puntiss <puntaandrea@gmail.com>
2022-04-27 09:49:55 +0200

    organize import

--- sondaggio/src/main/java/com/milano/sondaggio/config/SondaggiSecurityConfig.java
+++ sondaggio/src/main/java/com/milano/sondaggio/config/SondaggiSecurityConfig.java
@@ -3,7 +3,6 @@ package com.milano.sondaggio.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +14,7 @@ import com.milano.sondaggio.repository.UtenteRepository;
 @Configuration
 public class SondaggiSecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@SuppressWarnings("unused")
 	private UtenteRepository utenteRepository;
 	
 	public SondaggiSecurityConfig(UtenteRepository utenteRepository) {
